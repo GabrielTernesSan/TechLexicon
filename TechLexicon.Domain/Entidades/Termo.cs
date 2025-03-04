9ed243774ee5024a18232a215d89b80666c6f551
GabrielTernesSan <santos.tgabriel@hotmail.com>
2025-03-04 17:11:59 -0300

    Entidade principal

---
+++ TechLexicon.Domain/Entidades/Termo.cs
@@ -0,0 +1,14 @@
+﻿using TechLexicon.Domain.Enums;
+
+namespace TechLexicon.Domain.Entidades
+{
+    public class Termo
+    {
+        public int Id { get; }
+        public string Titulo { get; private set; } = null!;
+        public string Significado { get; private set; } = null!;
+        public ECategoria Categoria { get; private set; }
+        public DateTimeOffset DataPublicacao { get; set; }
+        public IEnumerable<string> Referencias { get; private set; } = null!;
+    }
+}
---
+++ TechLexicon.Domain/Enums/ECategoria.cs
@@ -0,0 +1,22 @@
+﻿namespace TechLexicon.Domain.Enums
+{
+    public enum ECategoria
+    {
+        CSharp = 1,
+        OO = 2,
+        DDD = 3,
+        Microsservicos = 4,
+        Postgres = 5,
+        Mongo = 6,
+        Redis = 7,
+        Docker = 8,
+        Kubernetes = 9,
+        Azure = 10,
+        Git = 11,
+        Angular = 12,
+        React = 13,
+        Oauth = 14,
+        JWT = 15,
+        Redes = 16
+    }
+}
